c270583d44e6d2ce8f6f58c462b551feb88da909
edoardob90 <edoardob90@gmail.com>
2022-12-01 10:13:56 +0100

    Update Docker action and normalize date formatting

--- .github/workflows/update_calendar.sh
+++ .github/workflows/update_calendar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-day=${1:-$(date +%d)}
+day=${1:-$(date +%-d)}
 year=${2:-$(date +%Y)}
 
 title=$(curl -s "https://adventofcode.com/$year/day/$day" | grep -o "<h2>.*</h2>" | sed -E "s/<h2>--- Day.*: (.*) ---<\/h2>/\1/g")
